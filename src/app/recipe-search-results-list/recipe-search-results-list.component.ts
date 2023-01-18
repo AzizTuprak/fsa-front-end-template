8476237eddf21392d8baf66f871afb55b64f1696
AzizTuprak <a.abdirshit@gmail.com>
2023-01-18 18:44:37 +0100

    creating related components

---
+++ src/app/recipe-search-results-list/recipe-search-results-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-search-results-list',
+  templateUrl: './recipe-search-results-list.component.html',
+  styleUrls: ['./recipe-search-results-list.component.css']
+})
+export class RecipeSearchResultsListComponent {
+
+}
